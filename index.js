054f649ecbb27f32672b25e19ca84781664bb184
Jennifer Devine <jennifermdevine88@gmail.com>
2023-06-21 18:27:24 -0700

    adding 404 page route

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const app = express();
+require('dotenv').config();
 
 app.get('/', (req, res) => {
     console.log('Root route hit!');
     res.send('<h1>Hello world!</h1>');
+})
+
+app.get('*', (req, res) => {
+    res.status(404).send('<h1>404 Page</h1>')
 })
 
 
@@ -15,8 +20,6 @@ app.get('/', (req, res) => {
 
 
 
-
-
-app.listen(3000, function() {
+app.listen(process.env.PORT, function() {
     console.log('Server is running!');
 })
