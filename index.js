e67d0250d56b35884d30c81ec82896e4a6dbe575
Jennifer Devine <jennifermdevine88@gmail.com>
2023-06-23 12:02:41 -0700

    adding routes table to README.md

--- index.js
+++ index.js
@@ -9,18 +9,18 @@ app.get('/', (req, res) => {
     res.send('<h1>Hello world!</h1>');
 })
 
+
+
+
+
+
+
+
+
+
 app.get('*', (req, res) => {
     res.status(404).send('<h1>404 Page</h1>')
 })
-
-
-
-
-
-
-
-
-
 
 app.listen(process.env.PORT, function() {
     console.log('Server is running!');
