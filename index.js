47b0937c920db27144590c85383d2f75ffeb8815
Jennifer Devine <jennifermdevine88@gmail.com>
2023-06-27 19:31:03 -0700

    added navbar and styling

--- index.js
+++ index.js
@@ -1,28 +1,29 @@
 require('dotenv').config();
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser')
+
+
+app.use(express.urlencoded({ extended: true }))
+app.use(express.static('public'))
+app.use('/places', require('./controllers/places'))
 
 app.set('views', __dirname + '/views')
 app.set('view engine', 'jsx');
 app.engine('jsx', require('express-react-views').createEngine());
-app.use(express.static('public'))
 
-app.use('/places', require('./controllers/places'))
 
-app.use(express.urlencoded({ extended: true }))
+
+
+
+
+app.use(bodyParser.json())
 
 
 app.get('/', (req, res) => {
     console.log('Root route hit!');
     res.render('home');
 })
-
-
-
-
-
-
-
 
 
 
