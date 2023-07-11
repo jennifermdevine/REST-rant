013ac9cbfd9d0e668a268204e9044517fbf3ce96
Jennifer Devine <jennifermdevine88@gmail.com>
2023-07-11 10:59:05 -0700

    finally got edit, delete working& comments started

--- controllers/places.js
+++ controllers/places.js
@@ -1,9 +1,13 @@
 const router = require('express').Router()
 const db = require('../models')
 
+
+// INDEX 
 router.get('/', (req, res) => {
     db.Place.find()
+    .populate('comments')
     .then((places) => {
+        console.log(places.comments)
     res.render('places/index', { places })
     })
     .catch(err => {
@@ -23,14 +27,18 @@ router.post('/', (req, res) => {
     })
 })
 
+// NEW
 
 router.get('/new', (req, res) => {
     res.render('places/new')
 })
 
+// SHOW
 router.get('/:id', (req, res) => {
     db.Place.findById(req.params.id)
+    .populate('comments')
     .then(place => {
+        console.log(place.comments)
         res.render('places/show', { place })
     })
     .catch(err => {
@@ -39,25 +47,84 @@ router.get('/:id', (req, res) => {
     })
 })
 
+//COMMENTS
 
-router.put('/:id', (req, res) => {
-  res.send('PUT /places/:id stub')
+router.post('/:id/comment', (req, res) => {
+    console.log('post comment', req.body)
+    if (req.body.author === '') { req.body.author = undefined }
+    req.body.rant = req.body.rant ? true : false
+    db.Place.findById(req.params.id)
+        .then(place => {
+            db.Comment.create(req.body)
+                .then(comment => {
+                    place.comments.push(comment.id)
+                    place.save()
+                        .then(() => {
+                            res.redirect(`/places/${req.params.id}`)
+                        })
+                        .catch(err => {
+                            res.render('error404')
+                        })
+                })
+                .catch(err => {
+                    res.render('error404')
+                })
+        })
+        .catch(err => {
+            res.render('error404')
+        })
 })
 
-router.delete('/:id', (req, res) => {
-  res.send('DELETE /places/:id stub')
+router.delete('/:id/comment/:commentId', (req, res) => {
+    db.Comment.findByIdAndDelete(req.params.commentId)
+        .then(() => {
+            console.log('Success')
+            res.redirect(`/places/${req.params.id}`)
+        })
+        .catch(err => {
+            console.log('err', err)
+            res.render('error404')
+        })
 })
+
+// EDIT
 
 router.get('/:id/edit', (req, res) => {
-  res.send('GET edit form stub')
+    db.Place.findById(req.params.id)
+        .then(place => {
+            res.render('places/edit', { place })
+        })
+        .catch(err => {
+            res.render('error404')
+        })
+})
+
+router.put('/:id', (req, res) => {
+    db.Place.findByIdAndUpdate(req.params.id, req.body)
+        .then(() => {
+            res.redirect(`/places/${req.params.id}`)
+        })
+        .catch(err => {
+            console.log('err', err)
+            res.render('error404')
+        })
 })
 
 router.post('/:id/rant', (req, res) => {
   res.send('GET /places/:id/rant stub')
 })
 
-router.delete('/:id/rant/:rantId', (req, res) => {
-    res.send('GET /places/:id/rant/:rantId stub')
+// DELETE
+
+router.delete('/:id', (req, res) => {
+    db.Place.findByIdAndDelete(req.params.id)
+        .then(() => {
+            res.redirect('/places')
+        })
+        .catch(err => {
+            console.log('err', err)
+            res.render('error404')
+        })
 })
 
 module.exports = router
--- views/places/show.jsx
+++ views/places/show.jsx
@@ -2,6 +2,23 @@ const React = require('react')
 const Def = require('../default')
 
 function show (data) {
+    let comments = (
+      <h3 className="inactive">No comments yet!</h3>
+    )
+    if (data.place.comments.length) {
+      comments = data.place.comments.map(c => {
+        return (
+          <div className="border">
+            <h2 className="rant">{c.rant ? 'Rant! 😡' : 'Rave! 😻'}</h2>
+            <h4>{c.content}</h4>
+            <h3>
+              <stong>- {c.author}</stong>
+            </h3>
+            <h4>Rating: {c.stars}</h4>
+          </div>
+        )
+      })
+    }
     return (
         <Def>
         <main>
@@ -10,13 +27,14 @@ function show (data) {
             <h3>{data.place.showEstablished()}</h3>
             <h4>Serving {data.place.cuisines}</h4>
             <h1>Currently Unrated</h1>
-            <h1>No Comments Yet!</h1>
+            <h1>Comments</h1>
+            <h2>{comments}</h2>
             <h3> Located in {data.place.city}, {data.place.state}</h3>
-            <a href={`/places/${data.id}/edit`} className="btn btn-warning"> 
+            <a href={`/places/${data.place.id}/edit`} className="btn btn-warning"> 
                 Edit
             </a>     
             
-            <form method="POST" action={`/places/${data.id}?_method=DELETE`}> 
+            <form method="POST" action={`/places/${data.place.id}?_method=DELETE`}> 
             <button type="submit" className="btn btn-danger">
                 Delete
             </button>
